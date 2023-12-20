82631f0677fa2701373b67cae27e349684cdbbed
niklas-kessler <95214248+niklas-kessler@users.noreply.github.com>
2023-12-20 16:28:21 +0100

    Fix Color Bug

--- components/message.tsx
+++ components/message.tsx
@@ -1,14 +1,14 @@
 export default function Message({content, sender}: {content:string, sender:string}) {
         
     const colorDict: Record<string, string> = {
-        "User": "blue-400",
-        "ChatGPT": "green-400",
-        "Tool": "red-400",
+        "User": "bg-blue-400",
+        "ChatGPT": "bg-green-400",
+        "Tool": "bg-red-400",
     }
 
     return(
         <div className="flex">
-            <div className={`h-10 bg-${colorDict[sender]} rounded-full mr-4`}>
+            <div className={`h-10 ${colorDict[sender]} rounded-full mr-4`}>
                 <div className="p-2"> {sender} </div>
             </div>
             <div className="bg-slate-300 w-full rounded">
