a20d5ccf0432915b9e3298e6b9100191c10a0590
niklas-kessler <95214248+niklas-kessler@users.noreply.github.com>
2023-12-29 00:17:58 +0100

    auto-scroll to bottom in chat

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,6 +1,7 @@
 import Message from "./message";
 import ChatInput from "./chat-input";
 import { MessageType } from "@/app/utils/types";
+import React,{ useEffect } from "react";
 
 interface ChatProps {
     messages: MessageType [],
@@ -8,9 +9,18 @@ interface ChatProps {
 }
 
 export default function Chat({ messages, appendMessage }: ChatProps) {
+
+    // Scroll to the bottom of the chat when messages change
+    const chatRef = React.createRef<HTMLDivElement>();
+    useEffect(() => {
+      if (chatRef.current) {
+        chatRef.current.scrollTop = chatRef.current.scrollHeight;
+      }
+    }, [messages]);
+
     return(
         <div className="flex-1 overflow-hidden flex flex-col justify-between">
-            <div className="flex-1 overflow-hidden space-y-10 overflow-y-auto px-4 py-4 max-h-full">
+            <div ref={chatRef} className="flex-1 overflow-hidden space-y-10 overflow-y-auto px-4 py-4 max-h-full">
                 {messages.map((element, index) => (
                     <div key={index}>
                         <Message sender={element.sender} content={element.content}/>
